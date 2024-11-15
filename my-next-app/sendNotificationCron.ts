a62321af642f4a7803e24c2ab5262c42b29c78b1
王后生 <houshengwang@trip.com>
2024-11-15 17:27:43 +0800

    Initial commit

---
+++ my-next-app/sendNotificationCron.ts
@@ -0,0 +1,38 @@
+// sendNotificationCron.ts
+require('dotenv').config();
+const cron = require('node-cron');
+const axios = require('axios');
+
+const appToken = process.env.WXPUSHER_TOKEN;
+const uid = process.env.WXPUSHER_UID;
+
+// 定义发送通知的函数
+const sendNotification = async () => {
+  try {
+    const url = 'http://wxpusher.zjiecode.com/api/send/message';
+
+    const request = {
+      appToken,
+      content: '亲爱的，已经很晚了，请注意休息，早点睡觉对身体好！ 🌙',
+      contentType: 1,
+      uids: [uid],
+    };
+
+    const response = await axios.post(url, request);
+
+    console.log('通知发送成功: ', new Date().toLocaleString());
+    console.log('API 响应: ', response.data);
+  } catch (error: any) {
+    console.error('发送通知失败: ', error.message);
+  }
+};
+
+// 设置定时任务
+cron.schedule('* * * * *', () => {
+  sendNotification();
+}, {
+  timezone: 'Asia/Shanghai'
+});
+
+// 立即执行一次
+sendNotification();
